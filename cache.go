96fc936e7578bf2463cfb343ebccf747aa7343bb
Patrick Klitzke <philologos14@gmail.com>
2023-09-27 09:37:13 +0900

    Add cache tests and function

--- cache.go
+++ cache.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"sync"
 	"time"
 )
 
@@ -12,21 +13,48 @@ type CacheItem struct {
 type Cache struct {
 	cacheMap map[string]CacheItem
 	duration time.Duration
+	mutex    sync.RWMutex
 }
 
 func NewCache(duration time.Duration) Cache {
-	return Cache{
+	var cache = Cache{
 		cacheMap: make(map[string]CacheItem),
 		duration: duration,
+	}
+
+	go cache.reapLoop()
+
+	return cache
+}
+
+func (ch *Cache) reapLoop() {
+	for {
+		ch.mutex.Lock()
+
+		for index, item := range ch.cacheMap {
+			res := time.Now().Sub(item.createdAt)
+
+			if res > ch.duration {
+				delete(ch.cacheMap, index)
+			}
+		}
+
+		ch.mutex.Unlock()
+
+		time.Sleep(ch.duration)
 	}
 }
 
 func (ch *Cache) Get(key string) ([]byte, bool) {
+	ch.mutex.Lock()
+	defer ch.mutex.Unlock()
 	val, ok := ch.cacheMap[key]
 	return val.val, ok
 }
 
 func (ch *Cache) Add(key string, value []byte) {
+	ch.mutex.Lock()
+	defer ch.mutex.Unlock()
 	ch.cacheMap[key] = CacheItem{
 		createdAt: time.Now(),
 		val:       value,
